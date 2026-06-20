fbd7fe09041adf0221d6a3097aacaa42dee44385
Maham Taqi <mahamtaqi3@gmail.com>
2026-06-21 03:19:44 +0500

    Fix comments and print statement in check_data.py

--- pipeline/check_data.py
+++ pipeline/check_data.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
 
-# Check karein ki npy file mein kya hai
+# Check what is prsent in npy file
 data = np.load('data/boletus_snp_matrix.npy')
 print(f"Data shape: {data.shape}")
 print(f"First 5 elements: {data[:5]}")
