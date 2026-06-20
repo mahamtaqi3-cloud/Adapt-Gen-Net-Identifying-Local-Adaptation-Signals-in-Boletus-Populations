0ad4e865b9c521f3a5ea3a5274e3f13a0eab5586
Maham Taqi <mahamtaqi3@gmail.com>
2026-06-21 03:24:50 +0500

    Update convert_gbff_to_gff.py

--- pipeline/convert_gbff_to_gff.py
+++ pipeline/convert_gbff_to_gff.py
@@ -1,17 +1,17 @@
 from Bio import SeqIO
 
 def convert_gbff_to_gff(input_file, output_file):
-    # Biopython ka use karke convert karein
+    # Convert using BioPython
     with open(output_file, "w") as out_handle:
         for record in SeqIO.parse(input_file, "genbank"):
             for feature in record.features:
                 if feature.type == "gene":
-                    # GFF format mein likhein
+                    # Write in GFF format
                     start = feature.location.start + 1
                     end = feature.location.end
                     gene_name = feature.qualifiers.get("gene", ["unknown"])[0]
                     out_handle.write(f"{record.id}\tgenbank\tgene\t{start}\t{end}\t.\t+\t.\tattribute={gene_name}\n")
     print("--- ✅ GBFF to GFF conversion done! ---")
 
-# Isay run karein
+# Run this
 convert_gbff_to_gff('data/genomic.gbff', 'data/genome.gff')
